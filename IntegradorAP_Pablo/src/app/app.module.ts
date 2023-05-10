e280e9bfac615c327cc5f77ff80327870af17b11
PabloCF2001 <pablofenankdes.12@gmail.com>
2023-05-09 22:18:35 -0300

    Se agregaron componentes nuevos(acerca, experiencia, skils,footer,educacion)

--- IntegradorAP_Pablo/src/app/app.module.ts
+++ IntegradorAP_Pablo/src/app/app.module.ts
@@ -6,17 +6,29 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { LogoAPComponent } from './componentes/logo-ap/logo-ap.component';
 import { AcercaComponent } from './componentes/acerca/acerca.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { SkilsComponent } from './componentes/skils/skils.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    AcercaComponent
+    AcercaComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkilsComponent,
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
